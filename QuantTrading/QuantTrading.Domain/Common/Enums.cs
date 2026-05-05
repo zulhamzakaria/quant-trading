d6bcc7bceb046aaa51e70b71186e8b3f62c7a0e6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-05-05 14:05:58 +0800

    - skeleton - this is the history of trade

---
+++ QuantTrading/QuantTrading.Domain/Common/Enums.cs
@@ -0,0 +1,21 @@
+﻿namespace QuantTrading.Domain.Common;
+
+public enum TradeSide
+{
+    None = 0,
+    Buy = 1,
+    Sell = 2
+}
+
+public enum SignalDirection
+{
+    Hold = 0,
+    Bullish = 1, // Price goes up
+    Bearish = 2  // Price goes down
+}
+
+public enum PositionStatus
+{
+    Open,
+    Closed
+}
--- QuantTrading/QuantTrading.Domain/Entities/Trade.cs
+++ QuantTrading/QuantTrading.Domain/Entities/Trade.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QuantTrading.Domain.Common;
+using System.Runtime.InteropServices;
 
-namespace QuantTrading.Domain.Entities
+namespace QuantTrading.Domain.Entities;
+
+public sealed class Trade
 {
-    internal class Trade
+    public Guid Id { get; private set; }
+    public string Symbol { get; private set; } = null!;
+    public decimal Price { get; private set; }
+    public decimal Quantity { get; private set; }
+    public TradeSide Side { get; private set; }
+    public DateTimeOffset Timestamp { get; private set; }
+
+    private Trade()
     {
+        //EF Core requires a parameterless constructor
     }
+
+    public static Trade Create(string symbol, decimal price, decimal quantity, TradeSide side)
+    {
+        return new Trade
+        {
+            Id = Guid.NewGuid(),
+            Symbol = symbol,
+            Price = price,
+            Quantity = quantity,
+            Side = side,
+            Timestamp = DateTimeOffset.UtcNow
+        };
+    }
+
 }
+
