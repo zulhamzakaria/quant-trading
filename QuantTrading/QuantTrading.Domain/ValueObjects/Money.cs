0a009cc604b97c1bc260848aa28789135dc4c4e0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-05-08 17:27:27 +0800

    - correctness fro Exception

--- QuantTrading/QuantTrading.Domain/ValueObjects/Money.cs
+++ QuantTrading/QuantTrading.Domain/ValueObjects/Money.cs
@@ -16,7 +16,7 @@ public sealed record Money(decimal Amount, Currency Currency)
     public static Money operator +(Money a, Money b)
     {
         if (a.Currency != b.Currency)
-            throw new Exception(DomainErrors.Money.CurrencyMismatch.ToString());
+            throw new InvalidOperationException(DomainErrors.Money.CurrencyMismatch.ToString());
 
         return new Money(a.Amount + b.Amount, a.Currency);
     }
