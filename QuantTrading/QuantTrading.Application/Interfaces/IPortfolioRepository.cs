1bc3191175f2933037f94044b8660d91ebe11656
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-05-15 12:13:01 +0800

    - reposr

--- QuantTrading/QuantTrading.Application/Interfaces/IPortfolioRepository.cs
+++ QuantTrading/QuantTrading.Application/Interfaces/IPortfolioRepository.cs
@@ -1,5 +1,11 @@
-﻿namespace QuantTrading.Application.Interfaces;
+﻿using QuantTrading.Domain.Entities;
+
+namespace QuantTrading.Application.Interfaces;
 
 public interface IPortfolioRepository
 {
+    Task<Portfolio?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    void Add(Portfolio portfolio);
+    void Update(Portfolio portfolio);
+    void Delete(Portfolio portfolio);
 }
---
+++ QuantTrading/QuantTrading.Infrastructure/Persistence/PortfolioRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuantTrading.Application.Interfaces;
+using QuantTrading.Domain.Entities;
+
+namespace QuantTrading.Infrastructure.Persistence;
+
+public class PortfolioRepository : IPortfolioRepository
+{
+    private readonly AppDbContext _context;
+    public PortfolioRepository(AppDbContext context)
+        => _context = context;
+    public async Task<Portfolio?> GetByIdAsync(Guid id, CancellationToken ct = default)
+    {
+        return await _context.Portfolios
+            .Include(p => p.Positions)
+            .FirstOrDefaultAsync(p => p.Id == id, ct);
+    }
+
+    public void Add(Portfolio portfolio)
+        => _context.Portfolios.Add(portfolio);
+
+    public void Delete(Portfolio portfolio)
+        => _context.Portfolios.Remove(portfolio);
+
+    public void Update(Portfolio portfolio)
+        => _context.Portfolios.Update(portfolio);
+}
