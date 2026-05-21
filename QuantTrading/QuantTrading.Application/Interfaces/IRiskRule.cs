36636236afce764ce560721aa76575204d53418b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-05-22 07:12:45 +0800

    - Risk management

---
+++ QuantTrading/QuantTrading.Application/Interfaces/IRiskRule.cs
@@ -0,0 +1,9 @@
+﻿using QuantTrading.Domain.Entities;
+using QuantTrading.Domain.Models;
+
+namespace QuantTrading.Application.Interfaces;
+
+public interface IRiskRule
+{
+    bool Allows(Signal signal, Portfolio portfolio, out string rejection);
+}
