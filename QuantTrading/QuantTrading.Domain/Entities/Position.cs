e7e4f0c670b5ca6b4ce51a9c9410abee133d3bc6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-05-22 09:34:56 +0800

    - added IsOpen, CostBasis and ClosePosition()

--- QuantTrading/QuantTrading.Domain/Entities/Position.cs
+++ QuantTrading/QuantTrading.Domain/Entities/Position.cs
@@ -5,7 +5,10 @@ public sealed class Position
     public Guid Id { get; private set; }
     public string Symbol { get; private set; } = null!;
     public decimal Quantity { get; private set; }
-    public decimal  AverageEntryPrice { get; private set; }
+    public decimal AverageEntryPrice { get; private set; }
+
+    public bool IsOpen => Quantity > 0;
+    public decimal CostBasis => Quantity * AverageEntryPrice;
 
     private Position() { }
 
@@ -18,30 +21,7 @@ public sealed class Position
         AverageEntryPrice = averageEntryPrice;
     }
 
-    /// <summary>
-    /// Average Entry Price	:
-    /// The weighted-average price you paid to acquire the current position. 
-    /// Used as the cost basis for P&L and tax calculations.
-    /// 
-    /// Cost Basis :
-    /// Total amount paid to acquire the asset. 
-    /// Selling part doesn't change the per-unit cost basis of what you still hold.
-    /// 
-    /// Delta :	
-    /// A signed change in quantity — positive for buy, negative for sell. 
-    /// "Delta" is overused in quant finance, but here it just means "change in position size."
-    /// 
-    /// Quantity :
-    /// How many units (shares, contracts, coins) you currently hold.
-    /// 
-    /// Weighted Average :	
-    /// Gives more influence to larger purchases.
-    /// 1 share@10 vs 1k shares@10
-    /// </summary>
-    /// <param name="delta"></param>
-    /// <param name="price"></param>
-
-    internal void UpdateQuantity(decimal delta, decimal price)
+    public void UpdateQuantity(decimal delta, decimal price)
     {
         if (delta > 0) // Case: Buying more of the same asset
         {
@@ -58,9 +38,12 @@ public sealed class Position
         }
     }
 
-    public decimal GetMarketValue(decimal currentPrice) 
+    public decimal GetMarketValue(decimal currentPrice)
         => Quantity * currentPrice;
 
     public decimal GetUnrealizedPnL(decimal currentPrice)
         => (currentPrice - AverageEntryPrice) * Quantity;
+
+    public void ClosePosition() 
+        => Quantity = 0;
 }
