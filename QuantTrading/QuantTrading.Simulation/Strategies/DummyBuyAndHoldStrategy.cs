10e6f7951044be59a0bdeff01522aa83c7d3b373
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-06-04 05:26:47 +0800

    - comment

--- QuantTrading/QuantTrading.Simulation/Strategies/DummyBuyAndHoldStrategy.cs
+++ QuantTrading/QuantTrading.Simulation/Strategies/DummyBuyAndHoldStrategy.cs
@@ -11,9 +11,9 @@ public sealed class DummyBuyAndHoldStrategy : IStrategy
 
     public OrderRequest? OnData(MarketData data, IReadonlyAccountState accountState)
     {
+        Console.WriteLine($"[Strategy] {data.Symbol} Position Size = {accountState.GetPositionSize(data.Symbol)}");
         if (!accountState.HasPositionOpen(data.Symbol))
         {
-            Console.WriteLine($"[Strategy] {data.Symbol} Position Size = {accountState.GetPositionSize(data.Symbol)}");
             return new OrderRequest(
                  data.Symbol,
                  OrderType.Market,
