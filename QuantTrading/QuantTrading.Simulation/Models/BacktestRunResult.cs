5afc1ba12f8fd7c96ffdfc83d53f0df2198444ad
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-06-05 09:59:48 +0800

    - added TotalReturnPercentage - missing TradeCount and WinRate (calculated from trades) still

--- QuantTrading/QuantTrading.Simulation/Models/BacktestRunResult.cs
+++ QuantTrading/QuantTrading.Simulation/Models/BacktestRunResult.cs
@@ -1,6 +1,7 @@
 ﻿using QuantTrading.Domain.Models;
 using QuantTrading.Domain.ValueObjects;
 using QuantTrading.Simulation.Execution;
+using QuantTrading.Simulation.Shared;
 
 namespace QuantTrading.Simulation.Models;
 
@@ -9,6 +10,10 @@ public sealed record BacktestRunResult(
     Money InitialCapital,
     Money FinalPortfolioValue,
     IReadOnlyCollection<EquityCurvePoint> EquityCurve,
-    IReadOnlyCollection<FillReceipt> Fills);
+    IReadOnlyCollection<FillReceipt> Fills)
+{
+    public decimal TotalReturnPercentage 
+        => (FinalPortfolioValue.Amount - InitialCapital.Amount) / InitialCapital.Amount * 100m;
+};
 
 
