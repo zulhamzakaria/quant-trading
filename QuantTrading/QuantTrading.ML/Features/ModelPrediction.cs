3809b912aac9184326cc629d887ffba4c1063d9f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-06-16 21:19:03 +0800

    - for Inferrence output

---
+++ QuantTrading/QuantTrading.ML/Features/ModelPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace QuantTrading.ML.Features;
+
+public sealed class ModelPrediction
+{
+    [ColumnName("PredictedLabel")]public bool PredictedLabel { get; set; }
+    [ColumnName("Probability")]public float Probability { get; set; }
+    [ColumnName("Score")]public float Score { get; set; }
+}
