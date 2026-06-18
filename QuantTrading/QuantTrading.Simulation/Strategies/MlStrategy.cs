aaa412c269f4108cf156bd2d1e0164ca98dbc096
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-06-18 21:03:20 +0800

    - need to add a new project to glue both ML and Simulation, effectively avoiding circular reference problem - WIP

---
+++ QuantTrading/QuantTrading.Simulation/Strategies/MlStrategy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ML;
+using QuantTrading.Domain.Models;
+using QuantTrading.Simulation.Contracts;
+using QuantTrading.Simulation.Models;
+
+namespace QuantTrading.Simulation.Strategies;
+
+public sealed class MlStrategy : IStrategy
+{
+    private readonly PredictionEngine<TrainingRow, ModelPrediction>
+    public string Name => throw new NotImplementedException();
+
+    public OrderRequest? OnData(MarketData data, IReadonlyAccountState accountState)
+    {
+        throw new NotImplementedException();
+    }
+}
