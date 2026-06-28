b8ad9b103eb3b3793b6cc7991b150609c981cb07
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-06-29 07:19:40 +0800

    - unused import

--- QuantTrading/QuantTrading.Simulation/Strategies/MlStrategy.cs
+++ QuantTrading/QuantTrading.Simulation/Strategies/MlStrategy.cs
@@ -3,7 +3,6 @@ using QuantTrading.Shared.Contracts;
 using QuantTrading.Shared.Execution;
 using QuantTrading.Shared.Features;
 using QuantTrading.Shared.Models;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace QuantTrading.Simulation.Strategies;
 
