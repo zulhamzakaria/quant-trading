2d9b2dbc247bf1396193a99fc70845bebd30d28f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-07-04 09:34:22 +0800

    - buy all on the first Open

---
+++ QuantTrading/QuantTrading.Simulation/Strategies/BuyAndHoldStrategy.cs
@@ -0,0 +1,27 @@
+﻿using QuantTrading.Shared.Contracts;
+using QuantTrading.Shared.Execution;
+using QuantTrading.Shared.Models;
+
+namespace QuantTrading.Simulation.Strategies;
+
+public sealed class BuyAndHoldStrategy : IStrategy
+{
+    public string Name => "Buy & Hold";
+
+    public OrderRequest? OnData(MarketData data, IReadonlyAccountState accountState)
+    {
+        if (accountState.HasPositionOpen(data.Symbol))
+            return null;
+
+        int quantity = (int)(accountState.Cash / data.Close);
+
+        if (quantity <= 0)
+            return null;
+
+        return new OrderRequest(
+            data.Symbol,
+            OrderType.Market,
+            OrderAction.Buy,
+            quantity);
+    }
+}
