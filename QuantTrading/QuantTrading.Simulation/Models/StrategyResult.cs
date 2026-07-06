19cb448a328f3bb2e2a02e1b37f5b034497426fc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-07-06 11:48:23 +0800

    - strats tourney

---
+++ QuantTrading/QuantTrading.Simulation/Models/StrategyResult.cs
@@ -0,0 +1,9 @@
+﻿namespace QuantTrading.Simulation.Models;
+
+public sealed record StrategyResult(
+    string StrategyName,
+    IReadOnlyList<CompletedTrade> Trades,
+    decimal StartingCapital,
+    decimal EndingPortfolioValue,
+    DateTime FirstBarTimestamp,
+    DateTime LastBarTimestamp);
