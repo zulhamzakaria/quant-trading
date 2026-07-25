a094b11ad461724cdd9ce49b41764234e39882b2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-07-25 19:16:11 +0800

    - auditing because of the "outrageous" numbers

--- QuantTrading/QuantTrading.ML/Program.cs
+++ QuantTrading/QuantTrading.ML/Program.cs
@@ -91,6 +91,7 @@ class Program
                 name: "ml-baseline-v2-2000"),
             new MlStrategy(
                 resolvedModelPath,
+                diagnosticMode: true,
                 allocationPerTrade: null,
                 confidenceMinPct: 0.10m,
                 confidenceMaxPct: 0.30m,
